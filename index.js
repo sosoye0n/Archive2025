ad55946135a3b6c8bd36d86837fd28eeb61560d9
sosoye0n <sosoyeon98@naver.com>
2025-08-28 14:14:42 +0900

    mainPage Update🕊️

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const selectedProjects = document.querySelector("#selectedProjects");
-const firstProject = document.querySelector(".first");
+// const selectedProjects = document.querySelector("#selectedProjects");
+// const firstProject = document.querySelector(".first");
 
-firstProject.addEventListener("click", () => {
-  window.location.href = "./pages/zip.html";
+// firstProject.addEventListener("click", () => {
+//   window.location.href = "./pages/zip.html";
+// });
+const projectLinks = [
+  "./pages/p01.html", // 첫 번째 li
+  "./pages/zip.html", // 첫 번째 li
+  "./pages/zip.html", // 첫 번째 li
+  "./pages/zip.html", // 첫 번째 li
+  "./pages/zip.html", // 첫 번째 li
+  "./pages/zip.html", // 첫 번째 li
+  "./pages/zip.html", // 첫 번째 li
+  "./pages/zip.html", // 첫 번째 li
+];
+
+const projectItems = document.querySelectorAll("#selectedProjects li");
+
+projectItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    window.location.href = projectLinks[index]; // 해당하는 링크로 이동
+  });
 });
