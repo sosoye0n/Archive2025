7af2850b7e65c35f1585d7df21fd1d383f705229
sosoye0n <sosoyeon98@naver.com>
2025-08-28 17:26:06 +0900

    lfontUpdate🕊️

--- category2.js
+++ category2.js
@@ -1,16 +1,16 @@
 const promotion = document.querySelectorAll(".projectBox");
 
 promotion[0].addEventListener("click", () => {
-  window.location.href = "./pages/p01.html";
+  window.location.href = "./pages/zip.html";
 });
 promotion[1].addEventListener("click", () => {
-  window.location.href = "./pages/p02.html";
+  window.location.href = "./pages/d02.html";
 });
 promotion[2].addEventListener("click", () => {
-  window.location.href = "./pages/p03.html";
+  window.location.href = "./pages/d03.html";
 });
 promotion[3].addEventListener("click", () => {
-  window.location.href = "./pages/p04.html";
+  window.location.href = "./pages/d04.html";
 });
 promotion[4].addEventListener("click", () => {
   window.location.href = "./pages/p05.html";
